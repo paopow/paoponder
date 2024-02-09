61e8e4ddd492c4301176373408159df449f24226
Pao Siangliulue <ksiangliulue@gmail.com>
2024-02-09 09:26:14 -0800

    Use a formal title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Paoponder.com",
+  title: "Pao Siangliulue",
   description: "Pao Siangliulue's personal website",
 };
 
