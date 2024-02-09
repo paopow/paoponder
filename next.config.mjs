c2c33e57dfb43419726cd31e4c6e6e538c542995
Pao Siangliulue <ksiangliulue@gmail.com>
2024-02-09 08:23:38 -0800

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // basePath: '/paopow.github.io',
-    // assetPrefix: '/paopow.github.io/',
-    images: {
-        unoptimized: true,
-    },
+    // output: 'export',
+    // // basePath: '/paopow.github.io',
+    // // assetPrefix: '/paopow.github.io/',
+    // images: {
+    //     unoptimized: true,
+    // },
 };
 
 export default nextConfig;
